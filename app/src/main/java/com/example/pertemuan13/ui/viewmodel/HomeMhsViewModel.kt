0aa2f82a8594ee28b35dae729820fcdd951c1a43
Jantopthegreat <fauzangriya@gmail.com>
2024-12-29 17:23:45 +0700

    membuat fungsi getMahasiswa

--- app/src/main/java/com/example/pertemuan13/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan13/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,11 +1,16 @@
 package com.example.pertemuan13.ui.viewmodel
 
+
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan13.model.Mahasiswa
 import com.example.pertemuan13.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 
 sealed class HomeMhsUiState{
@@ -15,6 +20,25 @@ sealed class HomeMhsUiState{
 }
 
 
-class HomeMhsViewModel(private val mhs: MahasiswaRepository) : ViewModel(){
+class HomeMhsViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
     var mhsUiState: HomeMhsUiState by mutableStateOf(HomeMhsUiState.Loading)
+        private set
+
+
+    init {
+        getMahasiswa()
+    }
+
+    fun getMahasiswa() {
+        viewModelScope.launch {
+            mhsUiState = HomeMhsUiState.Loading
+            mhsUiState = try {
+                HomeMhsUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException) {
+                HomeMhsUiState.Error
+            } catch (e: HttpException) {
+                HomeMhsUiState.Error
+            }
+        }
+    }
 }
