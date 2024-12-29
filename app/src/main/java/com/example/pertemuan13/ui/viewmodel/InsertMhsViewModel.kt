8e599cc49cc149a59b14062d3a3ebfe5f177cd47
Jantopthegreat <fauzangriya@gmail.com>
2024-12-29 16:52:12 +0700

    membuat fungsi toMhs dan class viewmodel lengkap

--- app/src/main/java/com/example/pertemuan13/ui/viewmodel/InsertMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan13/ui/viewmodel/InsertMhsViewModel.kt
@@ -4,13 +4,33 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan13.model.Mahasiswa
 import com.example.pertemuan13.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
 
-class InsertMhsViewModel (private val mhs : MahasiswaRepository) : ViewModel() {}
+class InsertMhsViewModel (private val mhs : MahasiswaRepository) : ViewModel() {
+    var uiState by mutableStateOf(InsertMhsUiState())
+        private set
+
+    fun updateInsertMhsState(insertMhsUiEvent : InsertMhsUiEvent) {
+        uiState = InsertMhsUiState(insertMhsUiEvent = insertMhsUiEvent)
+
+    }
 
 
+    suspend fun InsertMhs() {
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertMhsUiEvent.toMhs())
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 data class InsertMhsUiState (
     val insertMhsUiEvent : InsertMhsUiEvent = InsertMhsUiEvent()
@@ -25,6 +45,15 @@ data class InsertMhsUiEvent(
     val jenisKelamin: String = ""
 )
 
+fun InsertMhsUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan,
+    jenisKelamin = jenisKelamin
+)
+
 fun Mahasiswa.toUiStateMhs(): InsertMhsUiState = InsertMhsUiState(
     insertMhsUiEvent = InsertMhsUiEvent()
 )
@@ -35,5 +64,6 @@ fun Mahasiswa.toInsertMhsUiEvent(): InsertMhsUiEvent = InsertMhsUiEvent(
     alamat = alamat,
     kelas = kelas,
     angkatan = angkatan,
+    jenisKelamin = jenisKelamin
 )
 
