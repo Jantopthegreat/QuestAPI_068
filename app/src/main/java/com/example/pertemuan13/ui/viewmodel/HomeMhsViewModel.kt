d95f4d79d2041e66404c93ed685cdafd307d0e4f
Jantopthegreat <fauzangriya@gmail.com>
2024-12-29 17:09:00 +0700

    membuat class untuk viewmodel home

--- app/src/main/java/com/example/pertemuan13/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan13/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.pertemuan13.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.pertemuan13.model.Mahasiswa
+import com.example.pertemuan13.repository.MahasiswaRepository
 
 
 sealed class HomeMhsUiState{
@@ -10,4 +15,6 @@ sealed class HomeMhsUiState{
 }
 
 
-
+class HomeMhsViewModel(private val mhs: MahasiswaRepository) : ViewModel(){
+    var mhsUiState: HomeMhsUiState by mutableStateOf(HomeMhsUiState.Loading)
+}
